4673229566174db1383d86534cd03cb5d41099d5
BLinded-TD <ryanbkiyabu@gmail.com>
2026-07-16 00:24:03 -0500

    Created a vector class that can be used to more  easily draw vectors to the screen.

--- Mandelbrot/Mandelbrot.py
+++ Mandelbrot/Mandelbrot.py
@@ -1,23 +1,24 @@
 import numpy as np
 import pygame as pg
+from Vector_Class import Vector
 
 #initalizing scene
 pg.init()
-screen = pg.display.set_mode((640,360))
+screen = pg.display.set_mode((2*640,2*360))
 clock = pg.time.Clock()
 running = True
 dt,tt = 0,0
 center = pg.Vector2(screen.get_width()/2,screen.get_height()/2)
 #vector init
-vec_start = pg.Vector2(center,center)
-vec_end = pg.Vector2(center.x,center.y+50)
-vec1_angle = 0
-vec1_slope = 0
-k = 5
-r=5
-#Orthogonal Vector
+vec1_start = pg.Vector2(center.x,center.y)
+vec1_end = pg.Vector2(center.x,center.y+50)
+vec1 = Vector((center.x,center.y),(vec1_end.x,vec1_end.y),(200,0,100),10,10,20)
+vec2_start = pg.Vector2(vec1.arrow_tip().x,vec1.arrow_tip().y)
+vec2_end = pg.Vector2(50,50)
+vec2 = Vector(vec2_start,vec2_end,(256,256,256),vec1.line_width/2,5,10)
 #Text setup
 font = pg.font.Font(None,30)
+
 
 # session
 while running:
@@ -26,42 +27,30 @@ while running:
             running = False
     screen.fill(color = "#00000000")
     
-
     #vector graphic
-    vec1_dif = vec_end - vec_start
-    vec1_angle = np.arctan2(vec1_dif.y,vec1_dif.x)
-    orthog_angle = vec1_angle - np.pi/2
-    arrow_point_1 = pg.Vector2(vec_end.x,vec_end.y) - pg.Vector2(k*np.cos(orthog_angle),k*np.sin(orthog_angle))
-    arrow_point_2 = pg.Vector2(vec_end.x,vec_end.y) + pg.Vector2(k*np.cos(orthog_angle),k*np.sin(orthog_angle))
-    arrow_point_3 = pg.Vector2(vec_end.x,vec_end.y) + pg.Vector2(r*np.cos(vec1_angle),r*np.sin(vec1_angle))
-
+    vec1 = Vector((center.x,center.y),(vec1_end.x,vec1_end.y),(200,0,100),10,10,20)
+    vec2_start = pg.Vector2(vec1.arrow_tip().x,vec1.arrow_tip().y)
+    vec2 = Vector(vec2_start,vec2_end,(255,255,255),5,5,10)
+    vec1_angle = vec1.angle()
+    vec2_angle = vec2.angle()
     #display stuff here
-    vec1 = pg.draw.line(screen,"blue",
-                        vec_start,
-                        vec_end,3)
-    arrow_head = pg.draw.polygon(screen, "blue",
-                                 ((arrow_point_1),
-                                  (arrow_point_2),
-                                  (arrow_point_3)))
     floor = pg.draw.line(screen,"red",(0,center.y),(screen.get_width(),center.y) )
-    
-    
-    
-    
+    vec1.draw(screen)
+    vec2.draw(screen)
     #controls
     keys = pg.key.get_pressed()
     if keys[pg.K_a]:
-        vec_end.x -= 50 * dt
+        vec1_end.x -= 50 * dt
     if keys[pg.K_d]:
-        vec_end.x += 50 * dt
+        vec1_end.x += 50 * dt
     if keys[pg.K_w]:
-        vec_end.y -= 50 * dt
+        vec1_end.y -= 50 * dt
     if keys[pg.K_s]:
-        vec_end.y += 50 * dt
+        vec1_end.y += 50 * dt
     
     
     #Rendering Timer
-    text_surface = font.render(f"{tt:.02f}s" + f"{vec1_angle:.02f} deg",True,"blue")
+    text_surface = font.render(f"{tt:.02f}s" + f" {vec1_angle:.02f} deg" + f" {vec2_angle:.02f} deg",True,"blue")
     text_rect = text_surface.get_rect()
     text_rect.center = (screen.get_width()/2,screen.get_height()/4)
     screen.blit(text_surface,text_rect)
@@ -72,4 +61,3 @@ while running:
 
     pg.display.flip()
 pg.quit()
-
---
+++ Mandelbrot/Vector_Class.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pygame as pg
+class Vector:
+    def __init__ (self,start: tuple,end: tuple,color: tuple,line_width: float,arrow_width: float,arrow_height: float):
+        self.start = pg.Vector2(start)
+        self.end = pg.Vector2(end)
+        self.color = color
+        self.line_width = line_width
+        self.arrow_width = arrow_width
+        self.arrow_height = arrow_height
+    
+    
+
+    def difference(self):
+        return self.end - self.start
+   
+    def legnth(self):
+        return (self.difference()).length()
+
+    def angle(self):
+        return np.arctan2(self.difference().y,self.difference().x)
+
+    def arrow_tip(self):
+        return pg.Vector2((self.end.x + self.arrow_height*np.cos(self.angle())),(self.end.y + self.arrow_height*np.sin(self.angle())))
+
+
+    def draw(self,display):
+        #points for arrow's polygon
+        angle_perp = self.angle() - np.pi/2
+        arrow_point_1 = pg.Vector2((self.end.x - self.arrow_width*np.cos(angle_perp)),(self.end.y - self.arrow_width*np.sin(angle_perp)))
+        arrow_point_2 = pg.Vector2((self.end.x + self.arrow_width*np.cos(angle_perp)),(self.end.y + self.arrow_width*np.sin(angle_perp)))
+        arrow_point_3 = pg.Vector2((self.end.x + self.arrow_height*np.cos(self.angle())),(self.end.y + self.arrow_height*np.sin(self.angle())))
+
+        vector_line = pg.draw.line(display,self.color,self.start,self.end,self.line_width)
+        vector_arrow = pg.draw.polygon(display,self.color,((arrow_point_1),(arrow_point_2),(arrow_point_3)))
+
+
